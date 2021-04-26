c3dd41ecf0dd0d2dc9fc6aa29aa81f3feb1fcb5a
Aghora7 <ukikieran@gmail.com>
2021-04-27 00:27:37 +0530

    Olives: Move to google/xiaomi fingerprint

--- lineage_olives.mk
+++ lineage_olives.mk
@@ -22,9 +22,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8 Series
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.0.1.0.QCNINXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
